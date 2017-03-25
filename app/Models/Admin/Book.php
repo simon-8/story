df2a8fc28de5b2df31c7613b1ca8b82b095b4421
simon <liu@simon8.com>
2017-03-25 22:06:47 +0800

    修改小说页列表显示个数

--- app/Models/Admin/Book.php
+++ app/Models/Admin/Book.php
@@ -32,7 +32,7 @@ class Book extends Model
      * @param int $pagesize
      * @return mixed
      */
-    public function lists($condition = [] , $order = 'id DSEC',$pagesize = 12)
+    public function lists($condition = [] , $order = 'id DSEC',$pagesize = 10)
     {
         $order = $order ? explode(' ' , $order) : ['id' ,'DESC'];
         return $this->where( array_merge(['status' => 1],$condition) )->orderBy($order[0] , $order[1])->paginate($pagesize);
