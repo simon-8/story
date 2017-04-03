0da2bc05577a6260a805bd5e722344f9694c8b16
zidanfei <731886772@qq.com>
2017-04-03 17:46:05 +0800

    增加统计

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -12,7 +12,7 @@
         本站所有小说为转载作品，所有章节均由网友上传，转载至本站只是为了宣传本书让更多读者欣赏。
     </p>
     <p>
-        {{ $SET['powerby'] }}
+        {{ $SET['powerby'] }} <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1261657948'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1261657948%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
     </p>
 </div>
 <script>
