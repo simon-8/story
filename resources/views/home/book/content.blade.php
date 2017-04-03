483a45a4a7e1700b2e61029bdd3dc5467c23347c
zidanfei <731886772@qq.com>
2017-04-03 17:27:06 +0800

    修改栏目页分页数

--- resources/views/home/book/content.blade.php
+++ resources/views/home/book/content.blade.php
@@ -63,7 +63,90 @@
     {{--<div class="vote">--}}
         {{--<a id="a_uservote" href="javascript:;" rel="nofollow">确定</a>--}}
     {{--</div>--}}
-    <script>bgs()</script>
+    <div class="bgs">
+        <ul>
+            <li>
+                <input type="text" class="textm" id="screen" value="滚屏">
+                <input type="hidden" class="textm" id="screen2" value="滚屏">
+                <span class="btn" id="screen1"></span>
+            </li>
+            <li class="select">
+                <p>0</p>
+                <p>1慢</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <input type="text" class="textm" id="background" value="背景"/>
+                <input type="hidden" id="background2" value="#000"/>
+                <span class="btn" id="background1"></span>
+            </li>
+            <li class="select">
+                <p class="bg_huang">明黄</p>
+                <p class="bg_lan">淡蓝</p>
+                <p class="bg_lv">淡绿</p>
+                <p class="bg_fen">红粉</p>
+                <p class="bg_bai">白色</p>
+                <p class="bg_hui">灰色</p>
+                <p class="bg_hei">漆黑</p>
+                <p class="bg_cao">草绿</p>
+                <p class="bg_cha">茶色</p>
+                <p class="bg_yin">银色</p>
+                <p class="bg_mi">米色</p>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <input type="text" class="textm" id="fontSize" value="字号"/>
+                <input type="hidden" id="fontSize2" value="16px"/>
+                <span class="btn" id="fontSize1"></span>
+            </li>
+            <li class="select">
+                <p class="fon_12">12px</p>
+                <p class="fon_14">14px</p>
+                <p class="fon_16">16px</p>
+                <p class="fon_18">18px</p>
+                <p class="fon_20">20px</p>
+                <p class="fon_24">24px</p>
+                <p class="fon_30">30px</p>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <input type="text" class="textm" id="fontColor" value="字色"/>
+                <input type="hidden" id="fontColor2" value="z_mo"/>
+                <span class="btn" id="fontColor1"></span>
+            </li>
+            <li class="select">
+                <p class="z_hei">黑色</p>
+                <p class="z_red">红色</p>
+                <p class="z_lan">蓝色</p>
+                <p class="z_lv">绿色</p>
+                <p class="z_hui">灰色</p>
+                <p class="z_li">栗色</p>
+                <p class="z_wu">雾白</p>
+                <p class="z_zi">暗紫</p>
+                <p class="z_he">玫褐</p>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <input type="text" class="textm" id="fontFamily" value="字体"/>
+                <input type="hidden" id="fontFamily2" value="fam_song"/>
+                <span class="btn" id="fontFamily1"></span>
+            </li>
+            <li class="select"><p class="fam_song">宋体</p>
+                <p class="fam_hei">黑体</p>
+                <p class="fam_kai">楷体</p>
+                <p class="fam_qi">启体</p>
+                <p class="fam_ya">雅黑</p></li>
+        </ul>
+        <input type="button" class="ud_but2" onmousemove="this.className='ud_but22'" onmouseout="this.className='ud_but2'" value="保存" id="saveButton"/>
+        <input type="button" class="ud_but1" onmousemove="this.className='ud_but11'" onmouseout="this.className='ud_but1'" value="恢复" id="recoveryButton"/>
+    </div>
 </div>
 
 <div class="novel">
