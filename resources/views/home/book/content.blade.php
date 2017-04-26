678fda0e6e01d43c8032ed7fb013ad3567fcfbff
simon8 <liu@simon8.com>
2017-04-26 13:11:13 +0800

    ..

--- resources/views/home/book/content.blade.php
+++ resources/views/home/book/content.blade.php
@@ -204,7 +204,17 @@
 </div>
 
 <div class="yd_ad">
-
+    <script type="text/javascript">
+        document.write('<a style="display:none!important" id="tanx-a-mm_122370683_24304788_81438362"></a>');
+        tanx_s = document.createElement("script");
+        tanx_s.type = "text/javascript";
+        tanx_s.charset = "gbk";
+        tanx_s.id = "tanx-s-mm_122370683_24304788_81438362";
+        tanx_s.async = true;
+        tanx_s.src = "http://p.tanx.com/ex?i=mm_122370683_24304788_81438362";
+        tanx_h = document.getElementsByTagName("head")[0];
+        if(tanx_h)tanx_h.insertBefore(tanx_s,tanx_h.firstChild);
+    </script>
 </div>
 
 @include('home.footer')
