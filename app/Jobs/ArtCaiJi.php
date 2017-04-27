22e8fb8622107400f27515dbcd97f0cff505c3ef
zidanfei <731886772@qq.com>
2017-04-27 22:36:50 +0800

    采集加入Log记录

--- app/Jobs/ArtCaiJi.php
+++ app/Jobs/ArtCaiJi.php
@@ -37,98 +37,97 @@ class ArtCaiJi extends Job implements SelfHandling, ShouldQueue
      */
     public function handle()
     {
-        //if($this->attempts() > 3){
+        if( empty($this->Book['fromurl']) || empty($this->Book['id']) ){
+            \Log::debug(' --- 采集失败，Book DATA ---' . var_export($this->Book , true));
+            return true;
+        }
 
-        //}
-        \Log::debug(' --- ');
-        if( $this->Book['fromurl'] && $this->Book['id'] ){
+        //更新文章详情 / 缩略图
+        if(empty($this->Book['introduce']) || empty($this->Book['thumb'])){
 
-            //更新文章详情 / 缩略图
-            if(empty($this->Book['introduce']) || empty($this->Book['thumb'])){
+            $rules = [
+                'introduce' => [
+                    '.intro','text'
+                ],
+                'thumb' => [
+                    '.lf img','src'
+                ],
+            ];
+            $html = QueryList::Query($this->Book['fromurl'] , $rules , '' ,'UTF-8','GBK',true);
+            $bookInfo = $html->getData();
 
-                $rules = [
-                    'introduce' => [
-                        '.intro','text'
-                    ],
-                    'thumb' => [
-                        '.lf img','src'
-                    ],
-                ];
-                $html = QueryList::Query($this->Book['fromurl'] , $rules , '' ,'UTF-8','GBK',true);
-                $bookInfo = $html->getData();
-
-                $updateData = [];
-                if(empty($this->Book['introduce']) && !empty($bookInfo[0]['introduce'])){
-                    $updateData['introduce'] = $bookInfo[0]['introduce'];
-                }
-
-                if(empty($this->Book['thumb']) && !empty($bookInfo[0]['thumb']) && strpos($bookInfo[0]['thumb'],'nocover.jpg') === false){
-                    $thumb = save_remote_thumb($bookInfo[0]['thumb']);
-
-                    //上传到七牛
-                    $qiniuThumb = uploadToQiniu(public_path().$thumb);
-                    if($qiniuThumb){
-                        $updateData['thumb'] = $qiniuThumb;
-                        \File::delete(public_path().$thumb);
-                    }else{
-                        $updateData['thumb'] = $thumb;
-                    }
-                }
-
+            $updateData = [];
+            if(empty($this->Book['introduce']) && !empty($bookInfo[0]['introduce'])){
+                $updateData['introduce'] = $bookInfo[0]['introduce'];
             }
 
-            //获取章节列表
-            $rules = config('book.rules.88dushu.detail_list');
-            $book = $html->setQuery($rules);
-            $booksDetailLists = $book->getData();
+            if(empty($this->Book['thumb']) && !empty($bookInfo[0]['thumb']) && strpos($bookInfo[0]['thumb'],'nocover.jpg') === false){
+                $thumb = save_remote_thumb($bookInfo[0]['thumb']);
 
-            if(count($booksDetailLists)){
-
-                $baseUrl = $this->Book['fromurl'];
-
-                foreach($booksDetailLists as $k => &$v){
-                    $v = array_map('trim',$v);
-                    if(empty($v['fromurl'])){
-                        unset($booksDetailLists[$k]);
-                    }else{
-                        if(substr($v['fromurl'],0,4) !== 'http'){
-                            $v['fromurl'] = $baseUrl . $v['fromurl'];
-                        }
-                    }
-                }
-
-                $lastArticle = $this->getLastArticle($this->Book['id']);
-
-                if($lastArticle){
-                    $offset = 0;
-                    foreach($booksDetailLists as $k => $v)
-                    {
-                        if($v['title'] == $lastArticle->title){
-                            $offset = $k;
-                            break;
-                        }
-                    }
-                    //从最后一个章节开始截取10章
-                    $links = array_slice($booksDetailLists, $offset+1 , $this->Count);
+                //上传到七牛
+                $qiniuThumb = uploadToQiniu(public_path().$thumb);
+                if($qiniuThumb){
+                    $updateData['thumb'] = $qiniuThumb;
+                    \File::delete(public_path().$thumb);
                 }else{
-                    $links = array_slice($booksDetailLists, 0 , $this->Count);
+                    $updateData['thumb'] = $thumb;
                 }
+            }
 
-                $zhangjie = end($links);
-                if(is_array($zhangjie) && isset($zhangjie['title'])){
-                    $updateData['zhangjie'] = $zhangjie['title'];
-                }
-                $updateData['updated_at'] = date('Y-m-d H:i:s');
-                DB::table('books')->where('id',$this->Book['id'])->update($updateData);
+        }
 
-                foreach($links as $v){
-                    //推送到章节采集队列
-                    dispatch(
-                        new ArticleDetail( array_merge($v,['pid' => $this->Book['id']]) )
-                    );
+        //获取章节列表
+        $rules = config('book.rules.88dushu.detail_list');
+        $book = $html->setQuery($rules);
+        $booksDetailLists = $book->getData();
+
+        if(count($booksDetailLists)){
+
+            $baseUrl = $this->Book['fromurl'];
+
+            foreach($booksDetailLists as $k => &$v){
+                $v = array_map('trim',$v);
+                if(empty($v['fromurl'])){
+                    unset($booksDetailLists[$k]);
+                }else{
+                    if(substr($v['fromurl'],0,4) !== 'http'){
+                        $v['fromurl'] = $baseUrl . $v['fromurl'];
+                    }
                 }
+            }
+
+            $lastArticle = $this->getLastArticle($this->Book['id']);
+
+            if($lastArticle){
+                $offset = 0;
+                foreach($booksDetailLists as $k => $v)
+                {
+                    if($v['title'] == $lastArticle->title){
+                        $offset = $k;
+                        break;
+                    }
+                }
+                //从最后一个章节开始截取10章
+                $links = array_slice($booksDetailLists, $offset+1 , $this->Count);
+            }else{
+                $links = array_slice($booksDetailLists, 0 , $this->Count);
+            }
+
+            $zhangjie = end($links);
+            if(is_array($zhangjie) && isset($zhangjie['title'])){
+                $updateData['zhangjie'] = $zhangjie['title'];
+            }
+            $updateData['updated_at'] = date('Y-m-d H:i:s');
+            DB::table('books')->where('id',$this->Book['id'])->update($updateData);
+
+            foreach($links as $v){
+                //推送到章节采集队列
+                dispatch(
+                    new ArticleDetail( array_merge($v,['pid' => $this->Book['id']]) )
+                );
             }
         }
+
         return true;
     }
 
