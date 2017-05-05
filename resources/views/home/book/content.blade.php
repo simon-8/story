9923be12bb779aab4cf1886b5812884e8bd32bd7
simon8 <liu@simon8.com>
2017-05-05 13:52:22 +0800

    内容页左二广告位改为114联盟

--- resources/views/home/book/content.blade.php
+++ resources/views/home/book/content.blade.php
@@ -182,15 +182,7 @@
         </span>
         <span>
             <script type="text/javascript">
-                    document.write('<a style="display:none!important" id="tanx-a-mm_122370683_24304788_81714556"></a>');
-                    tanx_s = document.createElement("script");
-                    tanx_s.type = "text/javascript";
-                    tanx_s.charset = "gbk";
-                    tanx_s.id = "tanx-s-mm_122370683_24304788_81714556";
-                    tanx_s.async = true;
-                    tanx_s.src = "http://p.tanx.com/ex?i=mm_122370683_24304788_81714556";
-                    tanx_h = document.getElementsByTagName("head")[0];
-                    if(tanx_h)tanx_h.insertBefore(tanx_s,tanx_h.firstChild);
+                    document.write('<script src="http://js.centenr.net/page/s.php?s=467556&w=300&h=250"></script>');
             </script>
         </span>
         <span>
