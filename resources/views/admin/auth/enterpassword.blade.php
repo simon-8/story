3e2cf80d5ad50e42d8ae1f4c43458dd588ea88ba
simon8 <liu@simon8.com>
2017-06-20 16:09:14 +0800

    登录超时页细节调整

--- resources/views/admin/auth/enterpassword.blade.php
+++ resources/views/admin/auth/enterpassword.blade.php
@@ -1,34 +1,25 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="renderer" content="webkit">
-
     <title>登录超时</title>
-    <meta name="keywords" content="H+后台主题,后台bootstrap框架,会员中心主题,后台HTML,响应式后台">
-    <meta name="description" content="H+是一个完全响应式，基于Bootstrap3最新版本开发的扁平化主题，她采用了主流的左右两栏式布局，使用了Html5+CSS3等现代技术">
-
+    <meta name="keywords" content="">
+    <meta name="description" content="">
     <link href="/skin/manager/css/bootstrap.min.css?v=3.3.5" rel="stylesheet">
-    {{--<link href="/skin/manager/css/font-awesome.min.css?v=4.4.0" rel="stylesheet">--}}
-
     <link href="/skin/manager/css/animate.min.css" rel="stylesheet">
     <link href="/skin/manager/css/style.min.css?v=4.0.0" rel="stylesheet">
-
 </head>
-
 <body class="gray-bg">
+<div class="lock-word animated fadeInDown"></div>
 
-<div class="lock-word animated fadeInDown">
-</div>
 <div class="middle-box text-center lockscreen animated fadeInDown">
     <div>
         <div class="m-b-md">
             <img alt="image" class="img-circle circle-border" src="/skin/manager/img/profile_small.jpg">
         </div>
-        <h3>Beaut-zihan</h3>
+        <h3>嘿，您登录超时了</h3>
         <p>您需要再次输入密码</p>
         <form class="m-t" role="form" action="{{ route('getEnterpassword') }}" method="post">
             {!! csrf_field() !!}
@@ -46,11 +37,7 @@
         </form>
     </div>
 </div>
-
-<!-- 全局js -->
 <script src="/skin/js/jquery.min.js?v=2.1.4"></script>
 <script src="/skin/js/bootstrap.min.js?v=3.4.0"></script>
-
 </body>
-
 </html>
