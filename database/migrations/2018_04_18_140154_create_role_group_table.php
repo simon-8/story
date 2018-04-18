2e93684d4790fae06863735201b4007455de78fa
simon8 <liu@simon8.com>
2018-04-18 18:05:44 +0800

    添加RBAC所需表

---
+++ database/migrations/2018_04_18_140154_create_role_group_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rule_group', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->comment('角色名称');
+            $table->string('access')->comment('权限');
+            $table->unsignedTinyInteger('status')->default(0)->comment('状态');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_04_18_140507_create_role_access_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoleAccessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_access', function(Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('pid')->comment('上级权限');
+            $table->string('name')->comment('权限名称');
+            $table->string('path')->comment('权限路径');
+            //$table->name
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
