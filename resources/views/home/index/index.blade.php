720d2553bafcba0d3b611e86e53203c1c2fb018c
simon8 <liu@simon8.com>
2018-04-24 14:55:43 +0800

    添加googleadsense

--- resources/views/home/index/index.blade.php
+++ resources/views/home/index/index.blade.php
@@ -18,6 +18,13 @@
             location.href = '{!! wapurl() !!}';
         }
     </script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-2039171390518102",
+            enable_page_level_ads: true
+        });
+    </script>
 </head>
 <body>
 
